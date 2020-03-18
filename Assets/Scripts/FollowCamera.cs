5dfd1be36e6645fdef0bfaab4fc19526be4f213f
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-03-18 21:22:57 +0200

    Initial Commit
    - Created a sandbox environment
    - Created the player object
    - The player can move to the mouse cursor on mouse button 0 click

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    void Update()
+    {
+        transform.position = _target.position;
+    }
+}
---
+++ Assets/Scripts/PlayerMovementHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovementHandler : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private Ray _lastRay;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+
+
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo; // where the ray hit a colider
+        bool hasHit = Physics.Raycast(ray, out hitInfo);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hitInfo.point;
+        }
+    }
+}
